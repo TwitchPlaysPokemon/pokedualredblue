edd6189a6d0f61dc12e279c36f6dde5f2a44e2ab
Dave Rhodes <dave.was.r@gmail.com>
2017-12-21 06:59:48 +0000

    Trainers always use wTrainerName when speaking at the end of battle

--- engine/battle/save_trainer_name.asm
+++ engine/battle/save_trainer_name.asm
@@ -20,38 +20,39 @@ SaveTrainerName:
 
 TrainerNamePointers:
 ; what is the point of these?
-	dw YoungsterName
-	dw BugCatcherName
-	dw LassName
-	dw wTrainerName
-	dw JrTrainerMName
-	dw JrTrainerFName
-	dw PokemaniacName
-	dw SuperNerdName
+; for showing the trainer name at the end of a battle apparently
 	dw wTrainerName
 	dw wTrainerName
-	dw BurglarName
-	dw EngineerName
-	dw JugglerXName
-	dw wTrainerName
-	dw SwimmerName
 	dw wTrainerName
 	dw wTrainerName
-	dw BeautyName
-	dw wTrainerName
-	dw RockerName
-	dw JugglerName
 	dw wTrainerName
 	dw wTrainerName
-	dw BlackbeltName
 	dw wTrainerName
-	dw ProfOakName
-	dw ChiefName
-	dw ScientistName
 	dw wTrainerName
-	dw RocketName
-	dw CooltrainerMName
-	dw CooltrainerFName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
+	dw wTrainerName
 	dw wTrainerName
 	dw wTrainerName
 	dw wTrainerName
